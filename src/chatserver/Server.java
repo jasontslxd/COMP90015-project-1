5ce0c3d9ee32d269ddf7a2ce854b8d9c8cfffb60
jasontslxd <ckyjjt@gmail.com>
2021-08-20 16:44:21 +1000

    first commit

---
+++ src/chatserver/Server.java
@@ -0,0 +1,9 @@
+package chatserver;
+
+public class Server {
+    private static final int PORT = 4444;
+
+    public static void main(String[] args){
+        System.out.println("Hello world");
+    }
+}
